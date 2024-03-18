96675a9f5164df2f70075e06107d171167c21c9b
IonutG99 <ionutghimici@yahoo.com>
2024-03-18 14:54:11 +0200

    Refactor dictionary filter logic

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# input
 test_data1 = {
     "k1": "v1",
     "k2": ["v2", "v3", "", {"k21": "v21", "k22": ""}],
@@ -6,36 +7,34 @@ test_data1 = {
     "k10": {"k11": "", "k12": "v12"}
 }
 
+# desired output example ~ Remove all empty strings and None values from a dictionary
 # {"k1": "v1", "k2": ["v2", "v3", {"k21": "v21"}], "k3": {"k4": "v4", "k5": ["v5", "v6"], "k6": {"k7": "v7"}}, "k10": {"k12": "v12"}}
 
-
-def check_dict(value):
-    if value=="" or value==None:
-        return None
-    elif isinstance(value,dict):
-        r_value=value
-        for k,v in value.items():
-            return_value=check_dict(v)
-            if return_value==None:
-                del r_value[k]
-        return r_value
-    return value
-
+# case without list as a value of a key
 test_data2= {
     "k1": "v1",
-    # "k2": ["v2", "v3", "", {"k21": "v21", "k22": ""}],
-    # "k3": {"k4": "v4", "k5": ["v5", "v6", None], "k6": {"k7": "v7", "k8": ""}},
-    # "k9": None,
+    "k9": None,
     "k10": {"k11": "", "k12": "v12"}
 }
 
-output_dict=test_data2
-for key,value in test_data2.items():
-    return_value=check_dict(value)
-    if return_value==None:
-        del output_dict[key]
-        
-print(output_dict)
+def check_dict(value):
+    # filter out the empty strings and None values
+    if value=="" or value==None:
+        return None
+    # check if there's a nested dictionary
+    elif isinstance(value,dict):
+        r_value={}
+        for k,v in value.items():
+            return_value=check_dict(v)
+            # Add only non None values
+            if return_value is not None:
+                r_value[k]=return_value
+        return r_value
+    else:
+        return value
 
+output_dict2=check_dict(test_data2)
+print(output_dict2)
+# current output : {'k1': 'v1', 'k10': {'k12': 'v12'}}
         
     
